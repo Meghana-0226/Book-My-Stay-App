2b86f72333b6c5de5b2f2d16d8a38af0022c159d
Meghana-0226 <mohammedfarhanyusuf@gmail.com>
2026-03-09 09:51:37 +0530

    UC2- Basic Room Types & Static Availability

--- src/Main.java
+++
@@ -1,8 +0,0 @@
-package BookMyStay;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Welcome to the Hotel Booking Management system");
-        System.out.println("system intialised successfully");
-    }
-}
