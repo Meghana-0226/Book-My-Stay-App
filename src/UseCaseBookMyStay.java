e5d9b5a5a17c08f26993aedb6e5cf824a0d760b4
Meghana-0226 <mohammedfarhanyusuf@gmail.com>
2026-03-09 10:28:59 +0530

    UC6- Reservation Confirmation & Room Allocation

--- src/UseCaseBookMyStay.java
+++ src/UseCaseBookMyStay.java
@@ -1,7 +1,6 @@
 package BookMyStay;
 
-import java.util.Queue;
-import java.util.LinkedList;
+import java.util.*;
 class Reservation {
     String guestName;
     String roomType;
@@ -10,31 +9,67 @@ class Reservation {
         this.roomType = roomType;
     }
 }
-class BookingRequestQueue {
-    private Queue<Reservation> queue;
-    public BookingRequestQueue() {
-        queue = new LinkedList<>();
+class InventoryService {
+    private HashMap<String, Integer> inventory;
+    public InventoryService() {
+        inventory = new HashMap<>();
+        inventory.put("Single", 5);
+        inventory.put("Double", 3);
+        inventory.put("Suite", 2);
     }
-    public void addRequest(Reservation reservation) {
-        queue.add(reservation);
+    public int getAvailability(String type) {
+        return inventory.get(type);
     }
-    public void processRequests() {
+    public void decrement(String type) {
+        inventory.put(type, inventory.get(type) - 1);
+    }
+}
+class BookingService {
+    private InventoryService inventory;
+    private HashMap<String, Set<String>> allocatedRooms;
+    private HashMap<String, Integer> counters;
+    public BookingService(InventoryService inventory) {
+        this.inventory = inventory;
+        allocatedRooms = new HashMap<>();
+        counters = new HashMap<>();
+        allocatedRooms.put("Single", new HashSet<>());
+        allocatedRooms.put("Double", new HashSet<>());
+        allocatedRooms.put("Suite", new HashSet<>());
+        counters.put("Single", 1);
+        counters.put("Double", 1);
+        counters.put("Suite", 1);
+    }
+    public void processBookings(Queue<Reservation> queue) {
         while (!queue.isEmpty()) {
             Reservation r = queue.poll();
-            System.out.println(
-                    "Processing booking for Guest: " + r.guestName +
-                            ", Room Type: " + r.roomType
-            );
+            if (inventory.getAvailability(r.roomType) > 0) {
+                int id = counters.get(r.roomType);
+                String roomId = r.roomType + "-" + id;
+                Set<String> roomSet = allocatedRooms.get(r.roomType);
+                if (!roomSet.contains(roomId)) {
+                    roomSet.add(roomId);
+                    counters.put(r.roomType, id + 1);
+                    inventory.decrement(r.roomType);
+                    System.out.println(
+                            "Booking confirmed for Guest: " +
+                                    r.guestName +
+                                    ", Room ID: " +
+                                    roomId
+                    );
+                }
+            }
         }
     }
 }
 public class UseCaseBookMyStay {
     public static void main(String[] args) {
-        System.out.println("Booking Request Queue");
-        BookingRequestQueue bookingQueue = new BookingRequestQueue();
-        bookingQueue.addRequest(new Reservation("Abhi", "Single"));
-        bookingQueue.addRequest(new Reservation("Subha", "Double"));
-        bookingQueue.addRequest(new Reservation("Vanmathi", "Suite"));
-        bookingQueue.processRequests();
+        System.out.println("Room Allocation Processing");
+        Queue<Reservation> queue = new LinkedList<>();
+        queue.add(new Reservation("Abhi", "Single"));
+        queue.add(new Reservation("Subha", "Single"));
+        queue.add(new Reservation("Vanmathi", "Suite"));
+        InventoryService inventory = new InventoryService();
+        BookingService bookingService = new BookingService(inventory);
+        bookingService.processBookings(queue);
     }
 }
